ef3a3038001698c5fec6908382f0ef2fffddf004
krendell13 <ilove13@internet.ru>
2021-12-01 22:02:58 +0300

    create: mobile menu

--- js/main.js
+++ js/main.js
@@ -39,3 +39,11 @@ $(".newsletter").parallax({
   imageSrc: "img/newsletter-bg.jpg",
   speed: 0.4,
 });
+
+var menuButton = document.querySelector('.menu-button');
+menuButton.addEventListener("click", function() {
+  console.log("Клик по кнопке меню");
+  document
+    .querySelector(".navbar-bottom")
+    .classList.toggle('navbar-bottom--visible');
+});
