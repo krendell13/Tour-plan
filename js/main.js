3932eaddf1827a5b1685fb00dab742afc777e51d
krendell13 <ilove13@internet.ru>
2021-12-04 12:44:40 +0300

    add modal window

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-const hotelSlider = new Swiper('.hotel-slider', {
+$(document).ready(function () {
+  const hotelSlider = new Swiper('.hotel-slider', {
   // Optional parameters
   loop: true,
 
@@ -40,10 +41,28 @@ $(".newsletter").parallax({
   speed: 0.4,
 });
 
-var menuButton = document.querySelector('.menu-button');
-menuButton.addEventListener("click", function() {
-  console.log("Клик по кнопке меню");
-  document
-    .querySelector(".navbar-bottom")
-    .classList.toggle('navbar-bottom_visible');
+var menuButton = $('.menu-button');
+menuButton.on("click", function() {
+  $(".navbar-bottom").toggleClass('navbar-bottom_visible');
+});
+
+var modalButton = $('[data-toggle=modal]');
+var closeModalButton = $(".modal__close");
+modalButton.on('click', openModal);
+closeModalButton.on('click', closeModal);
+
+function openModal() {
+  var modalOverlay = $(".modal__overlay");
+  var modalDialog = $(".modal__dialog");
+  modalOverlay.addClass('modal__overlay--visible');
+  modalDialog.addClass('modal__dialog--visible');
+}
+function closeModal(event) {
+  event.preventDefault();
+  var modalOverlay = $(".modal__overlay");
+  var modalDialog = $(".modal__dialog");
+  modalOverlay.removeClass('modal__overlay--visible');
+  modalDialog.removeClass('modal__dialog--visible');
+}
+
 });
